bcb3dc3a04069f3fe77f2c098ede52f3c5051a14
Miguel Alcala <miguel.alcala@ykm.com.mx>
2022-11-25 17:37:03 -0600

    tablas relacionadas

--- app/Models/TransactionType.php
+++ app/Models/TransactionType.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TransactionType extends Model
 {
@@ -12,4 +13,15 @@ class TransactionType extends Model
     protected $fillable = [
         'tid', 'code', 'name', 'description', 'status',
     ];
+// relacion 1:n de outputs
+public function output():HasMany
+{
+    return $this->hasMany(Output::class);
+}
+// relacion 1:n de intputs
+public function intput():HasMany
+{
+    return $this->hasMany(Input::class);
+}
+
 }
---
+++ database/migrations/2022_11_25_165553_create_travel_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel', function (Blueprint $table) {
+            $table->id();
+            $table->string('carta_porte');
+            $table->string('unit_number');
+            $table->string('name');
+            $table->string('car_plates');
+            $table->string('car_operator');
+            $table->string('arrival_date');
+            $table->string('departure_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel');
+    }
+};
