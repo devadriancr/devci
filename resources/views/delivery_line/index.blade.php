e85b7ebe2a0c784fc5fc59afdc51c72993477329
Miguel Alcala <miguel.alcala@ykm.com.mx>
2023-01-11 15:36:51 -0600

    seguir escaneando

--- resources/views/delivery_line/index.blade.php
+++ resources/views/delivery_line/index.blade.php
@@ -46,6 +46,7 @@
                             <th class="px-4 py-3">Fecha</th>
                             <th class="px-4 py-3"></th>
                             <th class="px-4 py-3"></th>
+                            <th class="px-4 py-3"></th>
                         </tr>
                     </thead>
                     <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
@@ -64,7 +65,29 @@
                                 </td>
 
                                 <td class="px-4 py-3 text-sm">
-                                    Finalizado
+
+                                    @if ($travelss->finish !=1)
+
+                                    <form method="POST" action={{ route('Delivery.scanqr') }}>
+                                        @csrf
+
+                                        <input name="Delivery_id" value={{$travelss->id }} hidden>
+                                        <input name="location_id" value={{  $travelss->location_id}} hidden>
+                                        <div class="flex justify-end mt-2 gap-2">
+                                            <button
+                                                class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 013.75 9.375v-4.5zM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 01-1.125-1.125v-4.5zM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0113.5 9.375v-4.5z" />
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75zM6.75 16.5h.75v.75h-.75v-.75zM16.5 6.75h.75v.75h-.75v-.75zM13.5 13.5h.75v.75h-.75v-.75zM13.5 19.5h.75v.75h-.75v-.75zM19.5 13.5h.75v.75h-.75v-.75zM19.5 19.5h.75v.75h-.75v-.75zM16.5 16.5h.75v.75h-.75v-.75z" />
+                                                  </svg>
+
+                                                <span class="ml-2">Escanear</span>
+                                            </button>
+                                        </div>
+                                    </form>
+                                        @else
+                                         Entrega finalizado
+                                    @endif
                                 </td>
                                 <td class="px-4 py-3 text-sm">
 
@@ -87,6 +110,9 @@
 
 
                                 </td>
+
+
+
 
                             </tr>
                         @endforeach
