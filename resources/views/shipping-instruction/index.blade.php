439a473b029d8f07742801766600bd73c8150ba0
Jesus Adrian Camacho Rocha <jesus.camacho@ykm.com.mx>
2024-07-22 16:24:51 -0600

    ShippingInstruction (import): Validación de campos

--- resources/views/shipping-instruction/index.blade.php
+++ resources/views/shipping-instruction/index.blade.php
@@ -16,29 +16,33 @@
         @endif
 
         @if (session('success'))
-        <div class="bg-blue-100 border border-blue-400 text-blue-700 px-4 py-3 mb-2 rounded relative" role="alert">
-            <strong class="font-bold">¡Exitoso!</strong>
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 mb-2 rounded relative" role="alert">
+            <strong class="font-bold">¡Éxito!</strong>
             <span class="block sm:inline">{{ session('success') }}</span>
         </div>
         @endif
 
         @if (session('warning'))
-        <div class="bg-orange-100 border border-orange-400 text-orange-700 px-4 py-3 mb-2 rounded relative" role="alert">
-            <strong class="font-bold">¡Advertencia!</strong>
+        <div class="bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-3 mb-2 rounded relative" role="alert">
+            <strong class="font-bold">Advertencia</strong>
             <span class="block sm:inline">{{ session('warning') }}</span>
         </div>
         @endif
 
-        <!-- <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
-            Table with actions
-        </h4> -->
+        @if (session('error'))
+        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 mb-2 rounded relative" role="alert">
+            <strong class="font-bold">Error</strong>
+            <span class="block sm:inline">{{ session('error') }}</span>
+        </div>
+        @endif
+
         <div class="flex gap-2 px-4 py-3 mt-2 mb-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
             <div class="flex-auto">
                 <form method="POST" action="{{ route('shipping-instruction.store') }}" enctype="multipart/form-data">
                     @csrf
                     <label class="block text-sm">
                         <div class="relative text-gray-500 focus-within:text-purple-600">
-                            <input type="file" name="import_file" class="block w-full pr-20 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" placeholder="Jane Doe" />
+                            <input type="file" name="import_file" accept=".csv, .xlsx" class="block w-full pr-20 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" placeholder="Jane Doe" />
                             <button class="absolute inset-y-0 right-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-r-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
                                 Cargar
                             </button>
@@ -77,6 +81,7 @@
                 </div>
             </div>
         </div>
+
         <div class="w-full overflow-hidden rounded-lg shadow-xs">
             <div class="w-full overflow-x-auto">
                 <table class="w-full whitespace-no-wrap">
@@ -86,7 +91,7 @@
                             <th class="px-4 py-3">Factura</th>
                             <th class="px-4 py-3">Serial</th>
                             <th class="px-4 py-3">No. Parte</th>
-                            <th class="px-4 py-3">No. Parte</th>
+                            <th class="px-4 py-3">Cantidad Parte</th>
                             <th class="px-4 py-3">Fecha</th>
                             <th class="px-4 py-3">Hora</th>
                         </tr>
